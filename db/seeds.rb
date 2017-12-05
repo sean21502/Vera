04e93d08b4c8677edcffd6780daeb334aa1da932
Sean Hite <seanhite@gmail.com>
2017-12-04 19:37:56 -0500

    scss issue?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create!(
+  email: 'sean@gmail.com',
+  password: '123456',
+  confirmed_at: '2017-11-15'
+)
+
+users = User.all
+
+#Item seed
+10.times do
+	item = Item.create!(
+		user:  users.sample,
+		name:  Faker::Name.name,
+	)
+end
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} applications created"
